0d015e60bdd5d53bd2ecb273c7f5fa7c1e09dc55
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-17 16:42:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function createTable() {
-    //Write your code here
-  
+	function createTable() {
+		let numberRows = prompt("Input number of rows");
+       let numberCols = prompt("Input number of rows");
+		if(!numberCols || !numberRows)
+			alert("both row and cols are should be there")
+     const table = document.getElementById("myTable");	
+	const body = document.createElement("tbody");
+   for(let i=0;i<numberRows;i++)
+	{   
+		const row = document.createElement("tr");
+		for(let j=0;j<numberCols;j++){
+		const col = document.createElement("td");
+			col.innerHTML=`${i} ${j}`
+		row.appendChild(col);
+	    }
+	    body.appendChild(row);
+	}
+	table.appendChild(body);
 }
+
